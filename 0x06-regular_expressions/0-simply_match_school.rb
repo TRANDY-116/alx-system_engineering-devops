dd1f709d454572185381281e1351284bcbd606ab
TRANDY-116 <randyfun16@gmail.com>
2023-07-04 14:23:07 -0700

    0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#Create a Ruby script that accepts one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/Holberton/).join
+#Ruby script that accepts one argument and pass it to a regular expression matching method
+puts ARGV[0].scan(/School/).join
