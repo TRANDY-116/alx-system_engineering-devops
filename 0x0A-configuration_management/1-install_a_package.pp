d055adeb50f0e14e3ce0d4e110e9d600dbcb51f7
TRANDY-116 <randyfun16@gmail.com>
2024-01-20 08:55:52 +0100

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
 # Install a package "flask" in this case
+package { 'python3.8':
+ensure => '3.0.10',
+}
+
+package { 'python3-pip':
+ensure => installed,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+ensure   => '2.2.2',
+require  => Package['python3-pip'],
+provider => 'pip',
 }
