7428ab686477f50c44c93f7b0444bb559774dfb3
TRANDY-116 <randyfun16@gmail.com>
2024-03-07 15:53:07 +0100

    reddit sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""
-A function that queries the reddit api and return the number of subscribers
-for a given subredit
-"""
+""" function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0. """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """FUNCTION TO RETURN THE NUMBER OF SUBSCIBERS FOR A GIVEN SUBREDDIT"""
-    base_Url = "https://www.reddit.com/"
-    user_Url = f"{base_Url}/r/{subreddit}/about.json"
+    """function to return the number of subscribers for a given subreddit """
+    base_url = "https://www.reddit.com/"
+    user_url = f"{base_url}/r/{subreddit}/about.json"
 
-    user_agent = {"User-Agent": "Alx/advanced"}
+    user_agent = {"User-Agent": "Python/requests"}
 
-    response = requests.get(user_Url, headers=user_agent,
-                            allow_redirects=False, timeout=10)
+    response = requests.get(user_url, headers=user_agent,
+                            allow_redirects=False)
 
     if response.status_code in [301, 302, 400, 404]:
         return 0
 
-    return response.json().get('data').get("subscribers")
+    return response.json().get("data").get("subscribers")
+
