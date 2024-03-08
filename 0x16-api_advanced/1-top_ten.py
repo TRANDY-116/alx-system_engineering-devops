4e77790c3c06bb6da3f2259b8ac15a0d249ea1b6
TRANDY-116 <randyfun16@gmail.com>
2024-03-08 01:07:56 +0100

    another try2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+""" queries Reddit API and prints the titles """
+
+import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+    """
+    function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit
+    """
 
-    response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "Alx/requests"},
+    base_url = "https://www.reddit.com/"
+    user_url = f"{base_url}/r/{subreddit}/hot.json"
+
+    user_agent = {"User-Agent": "Python/requests"}
+
+    limits = {"limit": "10"}
+
+    response = requests.get(user_url, headers=user_agent, params=limits,
                             allow_redirects=False)
-    if response.status_code >= 300:
-        print('None')
+
+    if response.status_code in [301, 302, 400, 404]:
+        print("None")
     else:
-        [print(child.get("data").get("title"))
-         for child in response.json().get("data").get("children")]
+        json_respn = response.json()
+
+        if json_respn.get("data") and json_respn.get("data").get("children"):
+            hot_posts = json_respn.get("data").get("children")
+
+            for post in hot_posts:
+                if post.get("data") and post.get("data").get("title"):
+                    the_post = post.get("data").get("title")
+                    print(the_post)
