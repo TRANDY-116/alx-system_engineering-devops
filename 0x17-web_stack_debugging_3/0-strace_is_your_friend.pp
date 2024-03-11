1d3f08ec450491499fafee0192067635db8054bf
TRANDY-116 <randyfun16@gmail.com>
2024-03-11 03:17:30 +0100

    try

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# Fix the error file ext .phpp
+
+file { '/var/www/html/wp-settings.php':
+  ensure  => present
+}
+-> exec { 'replace':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
+}
